cdab57c88af64e76827c897466c4bdc88f067cda
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-05-07 08:57:09 -0500

    Added In Processing facet for Polaris grouped works.

--- code/reindexer/src/org/aspen_discovery/reindexer/GroupedWorkSolr2.java
+++ code/reindexer/src/org/aspen_discovery/reindexer/GroupedWorkSolr2.java
@@ -176,6 +176,7 @@ public class GroupedWorkSolr2 extends AbstractGroupedWorkSolr implements Cloneab
 			//Check to see if all items are on order.  If so, add on order keywords
 			boolean allItemsOnOrder = true;
 			boolean allItemsUnderConsideration = true;
+			boolean allItemsInProcess = true;
 			int numItems = 0;
 			HashSet<String> uniqueFormatCategories = new HashSet<>();
 			HashSet<String> uniqueFormats = new HashSet<>();
@@ -189,6 +190,9 @@ public class GroupedWorkSolr2 extends AbstractGroupedWorkSolr implements Cloneab
 							}
 							if (!item.getDetailedStatus().equals("Under Consideration")) {
 								allItemsUnderConsideration = false;
+							}
+							if (!item.getDetailedStatus().equals("In-Process")) {
+								allItemsInProcess = false;
 							}
 						}else{
 							allItemsOnOrder = false;
@@ -227,6 +231,10 @@ public class GroupedWorkSolr2 extends AbstractGroupedWorkSolr implements Cloneab
 				addKeywords("Coming Soon");
 				doc.addField("days_since_added", -1);
 				doc.addField("time_since_added", "On Order");
+			} else if (allItemsInProcess) {
+				addKeywords("In Processing");
+				doc.addField("days_since_added", -2);
+				doc.addField("time_since_added", "In Processing");
 			} else if (allItemsUnderConsideration) {
 				addKeywords("Under Consideration");
 				doc.addField("days_since_added", Integer.MAX_VALUE);
@@ -628,10 +636,17 @@ public class GroupedWorkSolr2 extends AbstractGroupedWorkSolr implements Cloneab
 	private Long daysAddedSincePubDate = null;
 	private Long loadScopedDaysAdded(ItemInfo curItem) {
 		Long daysSinceAdded;
-		if (curItem.isOrderItem() || (curItem.getStatusCode() != null && (curItem.getGroupedStatus().equals("On Order") || curItem.getDetailedStatus().equals("On Order") || curItem.getDetailedStatus().equals("Coming Soon") || curItem.getDetailedStatus().equals("Under Consideration")))) {
+		if (curItem.isOrderItem() || (curItem.getStatusCode() != null &&
+				(curItem.getGroupedStatus().equals("On Order") ||
+						curItem.getDetailedStatus().equals("On Order") ||
+						curItem.getDetailedStatus().equals("Coming Soon") ||
+						curItem.getDetailedStatus().equals("Under Consideration") ||
+						curItem.getDetailedStatus().equals("In-Process")))) {
 			if (curItem.getDetailedStatus().equals("Under Consideration")) {
 				daysSinceAdded = (long)Integer.MAX_VALUE;
-			}else{
+			} else if (curItem.getDetailedStatus().equals("In-Process")) {
+				daysSinceAdded = -2L;
+			} else {
 				daysSinceAdded = -1L;
 			}
 		} else {
