bca5d84fc22de1efe9218ef693399845ea464942
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-08-03 21:15:02 -0500

    Interface errors cleanup

--- code/web/services/GroupedWork/AJAX.php
+++ code/web/services/GroupedWork/AJAX.php
@@ -1077,7 +1077,7 @@ class GroupedWork_AJAX extends JSON_Action {
 		$interface->assign('shareItResults', $shareItResults['records']);
 
 		return [
-			'numTitles' => count($shareItResults),
+			'numTitles' => isset($shareItResults['records']) && is_array($shareItResults['records']) ? count($shareItResults['records']) : 0,
 			'formattedData' => $interface->fetch('GroupedWork/ajax-shareit.tpl'),
 		];
 	}
