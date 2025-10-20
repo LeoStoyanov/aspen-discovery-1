5521e23a53cc0dc59d2377b52782bb6c6a2e8baa
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-10-19 22:35:14 -0500

    Updated updateSuggesters.php cron job to rebuild suggest core.

--- code/web/cron/updateSuggesters.php
+++ code/web/cron/updateSuggesters.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../bootstrap.php';
 require_once ROOT_DIR . '/sys/CronLogEntry.php';
+
 $cronLogEntry = new CronLogEntry();
 $cronLogEntry->startTime = time();
 $cronLogEntry->name = 'Update Suggesters';
@@ -15,46 +16,20 @@ $opts = [
 	],
 ];
 $context = stream_context_create($opts);
-
 set_time_limit(0);
-require_once ROOT_DIR . '/sys/SystemVariables.php';
-$systemVariables = SystemVariables::getSystemVariables();
-if ($systemVariables->searchVersion == 1) {
-	if (!file_get_contents($solrBaseUrl . '/grouped_works/suggest?suggest.build=true', false, $context)) {
-		$cronLogEntry->notes .= "<br/>Could not update suggesters for grouped_works";
-		$cronLogEntry->numErrors++;
-	}else{
-		$cronLogEntry->notes .= "<br/>Updated suggesters for grouped_works";
-	}
+
+$suggestEndpoint = $solrBaseUrl . '/suggest/suggest?suggest.build=true';
+$cronLogEntry->notes = "Building suggester at $suggestEndpoint";
+$cronLogEntry->update();
+
+$result = @file_get_contents($suggestEndpoint, false, $context);
+if ($result === false) {
+	$cronLogEntry->notes .= "<br/>Could not rebuild suggester at $suggestEndpoint.";
+	$cronLogEntry->numErrors++;
 } else {
-	if (!file_get_contents($solrBaseUrl . '/grouped_works_v2/suggest?suggest.build=true', false, $context)) {
-		$cronLogEntry->notes .= "<br/>Could not update suggesters for grouped_works_v2";
-		$cronLogEntry->numErrors++;
-	}else{
-		$cronLogEntry->notes .= "<br/>Updated suggesters for grouped_works_v2";
-	}
+	$cronLogEntry->notes .= "<br/>Successfully triggered suggester rebuild at $suggestEndpoint.";
 }
-$cronLogEntry->update();
-if (!file_get_contents($solrBaseUrl . '/open_archives/suggest?suggest.build=true', false, $context)) {
-	$cronLogEntry->notes .= "<br/>Could not update suggesters for open_archives";
-	$cronLogEntry->numErrors++;
-}else{
-	$cronLogEntry->notes .= "<br/>Updated suggesters for open_archives";
-}
-$cronLogEntry->update();
-if (!file_get_contents($solrBaseUrl . '/genealogy/suggest?suggest.build=true', false, $context)) {
-	$cronLogEntry->notes .= "<br/>Could not update suggesters for genealogy";
-	$cronLogEntry->numErrors++;
-}else{
-	$cronLogEntry->notes .= "<br/>Updated suggesters for genealogy";
-}
-$cronLogEntry->update();
-if (!file_get_contents($solrBaseUrl . '/lists/suggest?suggest.build=true', false, $context)) {
-	$cronLogEntry->notes .= "<br/>Could not update suggesters for lists";
-	$cronLogEntry->numErrors++;
-}else{
-	$cronLogEntry->notes .= "<br/>Updated suggesters for lists";
-}
+
 $cronLogEntry->endTime = time();
 $cronLogEntry->update();
 
